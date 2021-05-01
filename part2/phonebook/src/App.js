16eb8ac8e56e0d8ece45508e71c4d68b9f981943
narsodev <narsodev@gmail.com>
2021-05-01 21:10:56 +0200

    move components to the folder "components"

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,8 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import Persons from './Persons.js';
-import PersonForm from './PersonForm.js';
-import Filter from './Filter.js';
+import Persons from './components/Persons.js';
+import PersonForm from './components/PersonForm.js';
+import Filter from './components/Filter.js';
 
 
 const App = () => {
